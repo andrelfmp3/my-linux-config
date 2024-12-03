844c95e6267a5f86189b80e1535007579e82a1c5
André Lyra <andre.lyra@protonmail.com>
2024-12-03 16:08:08 -0300

    update: readme pt-br + script lists

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# My-Arch_Config
+# my-linux-config
+
+#### Filosofia:
+- Poder dos customizáveis. Simplicidade dos pré-prontos.
+- Preferir open-source, mas não renegar software proprietário.
+- Batteries included. Básico que funciona.
+- Rolling release mais estável possível
+- Foco em terminal e atalhos de teclado (vim)
+- Aplicações maduras, evitar lançamentos novos. 
+- Sistema operacional deve solucionar problemas, e não ser um.
  
+#### Pontos Principais:
+- Arch Linux
+- KDE plasma
+- Kernel LTS 
+- Dracula Theme
+
+
---
+++ instalationScript.py
@@ -0,0 +1,41 @@
+instalationsExtra = [
+    "python-pip"
+    "firefox"
+    "vim"
+    "htop"
+    "git"
+    "blanket"
+    "yay"
+    "firefox"
+    "code" # vscode
+    "yakuake"
+    "konsole"
+    "zsh" # "zsh-syntax-highlighting", powelevel10k
+    "ufw"
+    "gufw"
+    "kdeconnect"
+    "spectacle"
+    "obsidian"
+    "netbeans"
+    "umbrello"
+    "mysql-workbench"
+    "umbrello"
+    "kolourpaint"
+    "libreoffice-still"
+    "tor"
+    "ncspot"
+    "yazi"
+    ]
+
+instalationsAur = [
+    "github-desktop-bin"
+    "spotify"
+    "qbittorrent"
+    "vesktop"
+    "mcomix"
+    "teamviewer"
+]
+
+instalationsMultilib = [
+    "steam"
+]
